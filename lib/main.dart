81becbf8e23014464345be8d9984c9b242b8b2a9
Kelvin Bush <kelybush@gmail.com>
2021-07-09 11:20:32 +0300

    randomising the dice twice once
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,11 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNo = 1;
   int rightDiceNo = 1;
 
+  void changeDiceNumber() => setState(() {
+        leftDiceNo = Random().nextInt(6) + 1;
+        rightDiceNo = Random().nextInt(6) + 1;
+      });
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -33,13 +38,11 @@ class _DicePageState extends State<DicePage> {
         children: [
           Expanded(
               child: TextButton(
-                  onPressed: () =>
-                      setState(() => leftDiceNo = Random().nextInt(6) + 1),
+                  onPressed: () => changeDiceNumber(),
                   child: Image.asset('images/dice$leftDiceNo.png'))),
           Expanded(
               child: TextButton(
-                  onPressed: () =>
-                      setState(() => rightDiceNo = Random().nextInt(6) + 1),
+                  onPressed: () => changeDiceNumber(),
                   child: Image.asset('images/dice$rightDiceNo.png'))),
         ],
       ),
