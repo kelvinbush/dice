17ca42ed1c823e4e4f2f050210d10781ca22ad98
Kelvin Bush <kelybush@gmail.com>
2021-07-09 09:56:43 +0300

    using the expanded widget
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,11 @@ void main() => runApp(
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(child: Image.asset('images/dice1.png')),
+        Expanded(child: Image.asset('images/dice1.png')),
+      ],
+    );
   }
 }
