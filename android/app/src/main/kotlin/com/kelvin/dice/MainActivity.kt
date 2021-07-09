ec2c672f25aabe806d3530dc7534cd86cb3d54e4
Kelvin Bush <kelybush@gmail.com>
2021-07-09 09:25:14 +0300

    starting files

---
+++ android/app/src/main/kotlin/com/kelvin/dice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kelvin.dice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Text('Dice'),
+            backgroundColor: Colors.red,
+          ),
+          body: DicePage(),
+        ),
+      ),
+    );
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
